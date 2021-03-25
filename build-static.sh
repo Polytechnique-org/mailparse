819a01abf2b12c5facee5995eef85471a60ed27e
Léo Gaspard <leo@gaspard.io>
2021-03-25 16:45:49 +0100

    Add infra to build static binary

---
+++ build-static.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+nix-shell -I nixpkgs=./common.nix -p crate2nix --run 'crate2nix generate --all-features'
+rm result
+nix-build -E '
+    (import ./common.nix {}).pkgsStatic.callPackage
+        ({ callPackage }: (
+            callPackage ./Cargo.nix {
+                buildRustCrateForPkgs = pkgs: pkgs.buildRustCrate;
+            }
+        ).rootCrate.build)
+        {}
+'
+rm Cargo.nix
--- common.nix
+++ common.nix
@@ -2,9 +2,9 @@
 
 let
   pkgsSrc = builtins.fetchTarball {
-    # The following is for nixos-unstable on 2020-12-13
-    url = "https://github.com/NixOS/nixpkgs/archive/e9158eca70ae59e73fae23be5d13d3fa0cfc78b4.tar.gz";
-    sha256 = "0cnmvnvin9ixzl98fmlm3g17l6w95gifqfb3rfxs55c0wj2ddy53";
+    # The following is for nixos-unstable on 2021-03-20
+    url = "https://github.com/NixOS/nixpkgs/archive/bc202733924c2b0f1a338f853de08c6a0459ba54.tar.gz";
+    sha256 = "0sfwng9gw97arvsn7dpdcawkn2mvnr2skhw6ppd3dkhjw2l2pavk";
   };
   pkgs = import pkgsSrc {
     overlays = [
